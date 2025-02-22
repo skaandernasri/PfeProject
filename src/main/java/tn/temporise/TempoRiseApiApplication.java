7937fc57318e4cb7b1c87da34ca3eecda4e8d1ad
Ahmed KHARRAZ <ahmed_kharraz@ext.carrefour.com>
2025-02-22 08:56:20 +0100

    add the project skeleton

---
+++ src/main/java/tn/temporise/TempoRiseApiApplication.java
@@ -0,0 +1,13 @@
+package tn.temporise.tempo_rise_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoRiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempoRiseApiApplication.class, args);
+	}
+
+}
